46818429b139f887ebf85df73ca25126b0aa7a1b
dbwls9675 <dbwls9675@nate.com>
2022-01-21 20:21:08 +0900

    데이터베이스 Connect

---
+++ saram.sql
@@ -0,0 +1,9 @@
+create table saram(
+no int not null primary key auto_increment,
+id varchar(20) not null,
+name varchar(20),
+age int
+);
+
+insert saram (id, name, age) values('Hong', 'gildong', 23);
+insert into saram (id, name, age) values('KIM','GILSUN',13);
